be3da59d4577d9c9ab4fe60c2fadb55bbeec4e93
pirobeem <pirobeem@gmail.com>
2023-11-14 22:34:16 +0900

    fix: add sample code

---
+++ src/sample.mjs
@@ -0,0 +1,17 @@
+#!/usr/bin/env zx
+$.verbose = false;
+
+const echoResult = await $`echo hello world`
+console.log(echoResult.stdout);
+
+// let branch = await $`git branch --show-current`
+// await $`dep deploy --branch=${branch}`
+
+// await Promise.all([
+//   $`sleep 1; echo 1`,
+//   $`sleep 2; echo 2`,
+//   $`sleep 3; echo 3`,
+// ])
+
+// let name = 'foo bar'
+// await $`mkdir /tmp/${name}`
